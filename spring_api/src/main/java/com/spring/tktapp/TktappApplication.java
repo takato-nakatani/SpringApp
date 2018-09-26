964f1967b7119ea99950a02b8c3b84c925cd2f97
takato-nakatani <syb01112@outlook.com>
2018-09-27 02:46:45 +0900

    request parameter test

--- spring_api/src/main/java/com/spring/tktapp/TktappApplication.java
+++ spring_api/src/main/java/com/spring/tktapp/TktappApplication.java
@@ -2,22 +2,12 @@ package com.spring.tktapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TktappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TktappApplication.class, args);
-		SpringApplication.run(TktappApplicationConfig.class, args);
-	}
-
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String testRest() {
-		return "rest tested";
+//		SpringApplication.run(TktappApplicationConfig.class, args);
 	}
 }
--- spring_api/src/main/java/com/spring/tktapp/TktappApplicationConfig.java
+++ spring_api/src/main/java/com/spring/tktapp/TktappApplicationConfig.java
@@ -1,13 +1,13 @@
 package com.spring.tktapp;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages={"com.Spring.controller"})
-@EntityScan("com.Spring.entity")
-@EnableJpaRepositories("com.Spring.repository")
-@RestController
-public class TktappApplicationConfig {
-}
+//@SpringBootApplication(scanBasePackages={"com.spring.controller"})
+//@EntityScan("com.spring.entity")
+//@EnableJpaRepositories("com.Spring.repository")
+//@RestController
+//public class TktappApplicationConfig {
+//}
