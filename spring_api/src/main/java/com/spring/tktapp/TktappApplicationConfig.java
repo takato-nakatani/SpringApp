338eafa47d5fe397dc8f361c04a917e934f32052
takato-nakatani <syb01112@outlook.com>
2018-09-27 01:06:11 +0900

    first commit

---
+++ spring_api/src/main/java/com/spring/tktapp/TktappApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.spring.tktapp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages={"com.Spring.controller"})
+@EntityScan("com.Spring.entity")
+@EnableJpaRepositories("com.Spring.repository")
+@RestController
+public class TktappApplicationConfig {
+}
---
+++ spring_api/src/test/java/com/spring/tktapp/TktappApplicationTests.java
@@ -0,0 +1,16 @@
+//package com.spring.tktapp;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class TktappApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
