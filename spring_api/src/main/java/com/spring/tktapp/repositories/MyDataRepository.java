6b05e5e9da11ae91a6c49d25114ef45664ea6acd
takato-nakatani <syb01112@outlook.com>
2018-09-30 03:43:58 +0900

    query annotation at repository class modified

--- spring_api/src/main/java/com/spring/tktapp/repositories/MyDataRepository.java
+++ spring_api/src/main/java/com/spring/tktapp/repositories/MyDataRepository.java
@@ -1,6 +1,7 @@
 package com.spring.tktapp.repositories;
 
 import com.spring.tktapp.entity.MyData;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -16,4 +17,8 @@ public interface MyDataRepository extends JpaRepository<MyData, Long> {
     //クエリアノテーションを使うことによってこのメソッドを呼び出す際に、このクエリが走ることになり、クエリ文の設定ができる。
     @Query("SELECT d from MyData d ORDER BY d.name")
     List<MyData> findAllOrderByName();
+
+    //クエリ文にパラメータを設定したいときは以下のようにすればよい
+    @Query("SELECT d from MyData d WHERE age > :min AND age < :max")
+    public List<MyData> finByAge(@Param("min") int min, @Param("max") int max);
 }
